690a3c644c257abbda885e5fbed732bb7cb6cce1
YomiCasual <yomionisade@gmail.com>
2020-03-18 12:24:08 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# threejs-tutorial
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/threejs-tutorial)
---
+++ index.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import * as THREE from "three";
+
+class App extends Component {
+  componentDidMount() {
+    // === THREE.JS CODE START ===
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+    var renderer = new THREE.WebGLRenderer( {antialias: true });
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+    var geometry = new THREE.BoxGeometry( 1,1,1);
+    // var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+
+    let texture = new THREE.TextureLoader().load('https://image.freepik.com/free-photo/blue-with-vignette-marble-texture-background-with-copy-space_23-2148327728.jpg')
+
+   var material = new THREE.MeshBasicMaterial( { map: texture} );
+
+    var cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+    camera.position.z = 5;
+
+    let speedX = 0.1
+    let speedY = 0.1
+
+    var animate = function () {
+      requestAnimationFrame( animate );
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      renderer.render( scene, camera );
+
+      cube.position.x += speedX
+      cube.position.y += speedY
+
+      if (cube.position.x > window.innerWidth/200) {
+        speedX = -0.1
+           }
+      if (cube.position.x < - (window.innerWidth/200)) {
+        speedX = 0.1
+      }
+
+      if (cube.position.y > window.innerHeight/250) {
+        speedY = -0.1      
+      }
+
+      if (cube.position.y < -(window.innerHeight/250)) {
+        speedY = 0.1
+      }
+    };
+    animate();
+
+    window.addEventListener('resize', () => {
+     camera.aspect = window.innerWidth / window.innerHeight;
+     camera.updateProjectionMatrix()
+     renderer.setSize( window.innerWidth, window.innerHeight );
+    })
+  }
+
+  resizeHandler = () => {
+
+  }
+  render() {
+    return (
+      <div />
+    )
+  }
+}
+render(<App />, document.getElementById('root'));
